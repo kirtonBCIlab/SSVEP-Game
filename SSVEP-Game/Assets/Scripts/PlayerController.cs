0036fe757465d8e5983e129d86423fc02c77d291
emilyschrag <emily.schrag@ucalgary.ca>
2025-05-07 12:59:37 -0700

    map laid out, no movement

--- SSVEP-Game/Assets/Scripts/PlayerController.cs
+++ SSVEP-Game/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,9 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
+        // Snap player to nearest grid cell
         currentGridPos = ground_tilemap.WorldToCell(transform.position);
+        Debug.Log("Start - Current Grid Position: " + currentGridPos);
         transform.position = ground_tilemap.GetCellCenterWorld(currentGridPos);
     }
 
@@ -20,14 +22,15 @@ public class PlayerController : MonoBehaviour
     {
         Vector2Int moveDirection = Vector2Int.zero;
 
-        if (Input.GetKeyDown(KeyCode.W)) // Up-Right
-            moveDirection = new Vector2Int(1, 1);
-        else if (Input.GetKeyDown(KeyCode.A)) // Up-Left
-            moveDirection = new Vector2Int(-1, 1);
-        else if (Input.GetKeyDown(KeyCode.S)) // Down-Right
-            moveDirection = new Vector2Int(1, -1);
-        else if (Input.GetKeyDown(KeyCode.D)) // Down-Left
-            moveDirection = new Vector2Int(-1, -1);
+        // Use standard grid directions
+        if (Input.GetKeyDown(KeyCode.W))        // Up
+            moveDirection = new Vector2Int(0, 1);
+        else if (Input.GetKeyDown(KeyCode.A))   // Left
+            moveDirection = new Vector2Int(-1, 0);
+        else if (Input.GetKeyDown(KeyCode.S))   // Down
+            moveDirection = new Vector2Int(0, -1);
+        else if (Input.GetKeyDown(KeyCode.D))   // Right
+            moveDirection = new Vector2Int(1, 0);
 
         if (moveDirection != Vector2Int.zero)
         {
@@ -38,17 +41,24 @@ public class PlayerController : MonoBehaviour
     private void Move(Vector2Int direction)
     {
         Vector3Int targetGridPos = currentGridPos + new Vector3Int(direction.x, direction.y, 0);
+        Debug.Log("Trying to move to: " + targetGridPos);
 
         if (CanMove(targetGridPos))
         {
             currentGridPos = targetGridPos;
             transform.position = ground_tilemap.GetCellCenterWorld(currentGridPos);
+            Debug.Log("Moved to: " + currentGridPos);
+        }
+        else
+        {
+            Debug.Log("Blocked: No tile at " + targetGridPos);
         }
     }
 
     private bool CanMove(Vector3Int targetGridPos)
     {
         TileBase tile = ground_tilemap.GetTile(targetGridPos);
+        Debug.Log($"Tile at {targetGridPos}: {(tile != null ? tile.name : "null")}");
         return tile != null;
     }
 }
